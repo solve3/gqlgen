6a65851ca96243d8fbc757b415819f4f95334e8b
solve <1195811799@qq.com>
2025-03-05 09:37:54 +0800

    引用本地静态文件

--- graphql/playground/playground.go
+++ graphql/playground/playground.go
@@ -24,18 +24,18 @@ var page = template.Must(template.New("graphiql").Parse(`<!DOCTYPE html>
 		}
 	</style>
 	<script
-		src="https://cdn.jsdelivr.net/npm/react@18.2.0/umd/react.production.min.js"
+		src="./statics/react.production.min.js"
 		integrity="{{.reactSRI}}"
 		crossorigin="anonymous"
 	></script>
 	<script
-		src="https://cdn.jsdelivr.net/npm/react-dom@18.2.0/umd/react-dom.production.min.js"
+		src="./statics/react-dom.production.min.js"
 		integrity="{{.reactDOMSRI}}"
 		crossorigin="anonymous"
 	></script>
     <link
 		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/graphiql@{{.version}}/graphiql.min.css"
+		href="./statics/graphiql.min.css"
 		integrity="{{.cssSRI}}"
 		crossorigin="anonymous"
 	/>
@@ -44,7 +44,7 @@ var page = template.Must(template.New("graphiql").Parse(`<!DOCTYPE html>
     <div id="graphiql">Loading...</div>
 
 	<script
-		src="https://cdn.jsdelivr.net/npm/graphiql@{{.version}}/graphiql.min.js"
+		src="./statics/graphiql.min.js"
 		integrity="{{.jsSRI}}"
 		crossorigin="anonymous"
 	></script>
